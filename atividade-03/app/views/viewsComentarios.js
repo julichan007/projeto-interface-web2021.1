e88a0dcf94a0b48956e2ccb024a8362e0743280f
Juliana Barreto <julichan.sousa@gmail.com>
2021-07-21 23:22:37 -0300

    Inicio da Atividade 03
    Inserido a pasta da atividade 03. Autenticação

---
+++ atividade-03/app/views/viewsComentarios.js
@@ -0,0 +1,15 @@
+function render (comentario){
+    return{
+        id: comentario._id,
+        texto: comentario.texto,
+        id_post: comentatio.id_post,
+        id_usuario: comentario.id_usuario
+    }
+}
+
+module.exports.render = render;
+
+function renderMany(comentarios){
+    return comentarios.map(render)
+}
+module.exports.renderMany = renderMany;
---
+++ atividade-03/config/express.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const routerUsuarios = require("../app/routes/routesUsuarios")
+const bodyParser = require("body-parser")
+//import bodyParser from 'body-parser'
+
+const routerPosts = require("../app/routes/routesPosts")
+const routerComentarios = require("../app/routes/routesComentario")
+
+module.exports = function(){
+    let app = express()
+
+    app.set("port", 8393)
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(express.static('./public'))
+    
+    routerUsuarios(app)
+    routerComentarios(app)
+    routerPosts(app)
+    
+
+
+    return app;
+};
